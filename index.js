5bdda007d4a05cc23409f55336ff44678a06ccb7
NumberCooler <fsh.kod@gmail.com>
2018-01-05 22:11:44 -0200

    consolidate
    1

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
-//require
 var dp = require("./lib/DiskProxy");
 var fs = require("fs");
 var repl = require("repl");
-var express = require("express");
-var request = require("request");
-var webfile = require("./lib/WebFile");
-
-//alias
 var DiskProxy = dp.DiskProxy;
 var dump = dp.dump;
 var memory = dp.mem;
-//set
 var context = {};
 var swap = dp.setSwap(DiskProxy("swap","object",null));
 context.rt = swap;
 var rt = dp.getSwap();
-
 var ram = {};
-
 context.ram = ram;
 var lib = {
-	fs : fs,
-	express : express,
-	express_util : {
-		routeHandler : function(options) {
-			if( options.type == "file" ) {
-				return function(req,res) {
-					options.box.filepath
-				}
-			}
-		}
-	},
-	webfile : webfile,
-	request : request,
-	lang : {
-			utils : {
-					runExtended : function(target,name,funktionList) {
-							var a;
-							target[name] = function() {
-								return target["$"+name].run($);
-							}
-							if( arguments.length < 3 ) {
-								console.log("target name functionList...");
-								return;
-							}
-							var args = [];
-							for(var x = 2; x < arguments.length;x++) {
-								args.push(arguments[x]);
-							}
-							target["$" + name ] = {
-								data : args,
-								run : function(ctx) {
-									this.target = {};
-									var base = this.target;
-									for(var x = 0; x < this.data.length;x++) {
-										base = this.data[x](ctx,base);
-									}
-									return base;
-								}
-							};
-							
-					}
-			}
-	}
+	fs : fs
 };
 context.lib = lib;
 var diskproxy = DiskProxy("disk/alpha","object",context);
@@ -113,33 +62,30 @@ function initialize() {
 	  value: documentation
 	});
 
-	Object.defineProperty(r.context, 'dump', {
-	  configurable: false,
-	  enumerable: true,
-	  value: dump
-	});
-
-	Object.defineProperty(r.context, 'memory', {
-	  configurable: false,
-	  enumerable: true,
-	  value: memory
-	});
-
 	Object.defineProperty(r.context, 'rt', {
 	  configurable: false,
 	  enumerable: true,
 	  value: rt
 	});
-	
-	
-	diskproxy.boot.start(diskproxy,r.context);
+	function set(key,val) {
+		Object.defineProperty(r.context, key, {
+		  configurable: false,
+		  enumerable: true,
+		  value: val
+		});
+	}
+	for(var key in diskproxy.alias) {
+		var $ = r.context;
+		eval("var f = " + diskproxy.alias[key]);
+		set(key,f);
+	}
 }
 initialize();
 r.on('exit', () => {
-  diskproxy.shutdown.start(diskproxy,r.context);
-  process.exit();
+	// shutdown
+	process.exit();
 });
 r.on('reset',() => {
-	diskproxy.shutdown.start(diskproxy,r.context);
+	// shutdown
 	initialize();
 });
